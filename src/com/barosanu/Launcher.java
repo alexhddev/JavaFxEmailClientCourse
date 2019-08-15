31b43f445fd1f867e9c051c3699df00759ab9852
barosanuemailtest <barosanu.email.test@gmail.com>
2019-08-15 12:25:09 +0300

    Css selectors

--- src/com/barosanu/Launcher.java
+++ src/com/barosanu/Launcher.java
@@ -19,7 +19,8 @@ public class Launcher extends Application {
     public void start(Stage stage) throws Exception {
 
         ViewFactory viewFactory = new ViewFactory(new EmailManager());
-        viewFactory.showLoginWindow();
+        viewFactory.showOptionsWindow();
+        viewFactory.updateStyles();
 
     }
 }
--- src/com/barosanu/view/ViewFactory.java
+++ src/com/barosanu/view/ViewFactory.java
@@ -24,7 +24,7 @@ public class ViewFactory {
     }
 
     //View options handling:
-    private ColorTheme colorTheme = ColorTheme.DEFAULT;
+    private ColorTheme colorTheme = ColorTheme.DARK;
     private FontSize fontSize = FontSize.MEDIUM;
 
     public ColorTheme getColorTheme() {
