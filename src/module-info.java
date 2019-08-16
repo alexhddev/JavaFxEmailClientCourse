244ce5e4333e7e75ca93da0d32277f9ec5e40417
barosanuemailtest <barosanu.email.test@gmail.com>
2019-08-16 12:24:45 +0300

    JavaMail setup

--- src/module-info.java
+++ src/module-info.java
@@ -3,6 +3,8 @@ module JavaFxEmailClientCourse {
     requires javafx.controls;
     requires javafx.graphics;
     requires javafx.web;
+    requires activation;
+    requires java.mail;
 
     opens com.barosanu;
     opens com.barosanu.view;
