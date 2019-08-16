cada8a1bb94a7aeb652bccc7e695809ffbb28643
barosanuemailtest <barosanu.email.test@gmail.com>
2019-08-16 14:20:51 +0300

    Login action

--- src/com/barosanu/controller/LoginWindowController.java
+++ src/com/barosanu/controller/LoginWindowController.java
@@ -1,6 +1,8 @@
 package com.barosanu.controller;
 
 import com.barosanu.EmailManager;
+import com.barosanu.controller.services.LoginService;
+import com.barosanu.model.EmailAccount;
 import com.barosanu.view.ViewFactory;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -25,9 +27,34 @@ public class LoginWindowController extends BaseController {
 
     @FXML
     void loginButtonAction() {
+        if(fieldsAreValid()){
+            EmailAccount emailAccount = new EmailAccount(emailAddressFied.getText(), passwordField.getText());
+            LoginService loginService = new LoginService(emailAccount, emailManager);
+            EmailLoginResult emailLoginResult= loginService.login();
+
+            switch (emailLoginResult) {
+                case SUCCESS:
+                    System.out.println("login succesfull!!!" + emailAccount);
+                    return;
+            }
+        }
         System.out.println("loginButtonAction!!");
         viewFactory.showMainWindow();
         Stage stage = (Stage) errorLabel.getScene().getWindow();
         viewFactory.closeStage(stage);
+    }
+
+    private boolean fieldsAreValid() {
+        if(emailAddressFied.getText().isEmpty()) {
+            errorLabel.setText("Please fill email");
+            return false;
+        }
+        if(passwordField.getText().isEmpty()) {
+            errorLabel.setText("Please fill password");
+            return false;
+        }
+
+        return true;
+
     }
 }
--- src/com/barosanu/controller/services/LoginService.java
+++ src/com/barosanu/controller/services/LoginService.java
@@ -4,6 +4,8 @@ import com.barosanu.EmailManager;
 import com.barosanu.controller.EmailLoginResult;
 import com.barosanu.model.EmailAccount;
 
+import javax.mail.*;
+
 public class LoginService {
 
     EmailAccount emailAccount;
@@ -15,6 +17,33 @@ public class LoginService {
     }
 
     public EmailLoginResult login(){
+        Authenticator authenticator = new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(emailAccount.getAddress(), emailAccount.getPassword());
+            }
+        };
 
+        try {
+            Session session = Session.getInstance(emailAccount.getProperties(), authenticator);
+            Store store = session.getStore("imaps");
+            store.connect(emailAccount.getProperties().getProperty("incomingHost"),
+                    emailAccount.getAddress(),
+                    emailAccount.getPassword());
+            emailAccount.setStore(store);
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+            return EmailLoginResult.FAILED_BY_NETWORK;
+        } catch (AuthenticationFailedException e) {
+            e.printStackTrace();
+            return  EmailLoginResult.FAILED_BY_CREDENTIALS;
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            return EmailLoginResult.FAILED_BY_UNEXPECTED_ERROR;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return  EmailLoginResult.FAILED_BY_UNEXPECTED_ERROR;
+        }
+        return EmailLoginResult.SUCCESS;
     }
 }
