55bcbd7aab4c737f2152aa05f0a3352ca60e764d
barosanuemailtest <barosanu.email.test@gmail.com>
2020-04-14 16:32:40 +0300

    Send emails with attachments

--- src/com/barosanu/controller/ComposeMessageController.java
+++ src/com/barosanu/controller/ComposeMessageController.java
@@ -10,12 +10,18 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.web.HTMLEditor;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class ComposeMessageController extends BaseController implements Initializable {
+
+    private List<File> attachments = new ArrayList<File>();
 
     @FXML
     private TextField recipientTextFIeld;
@@ -32,6 +38,15 @@ public class ComposeMessageController extends BaseController implements Initiali
     @FXML
     private ChoiceBox<EmailAccount> emailAccountChoice;
 
+    @FXML
+    void attachBtnAction() {
+        FileChooser fileChooser = new FileChooser();
+        File selectedFile = fileChooser.showOpenDialog(null);
+        if(selectedFile != null){
+            attachments.add(selectedFile);
+        }
+    }
+
 
     @FXML
     void sendButtonAction() {
@@ -39,7 +54,8 @@ public class ComposeMessageController extends BaseController implements Initiali
                 emailAccountChoice.getValue(),
                 subjectTextField.getText(),
                 recipientTextFIeld.getText(),
-                htmlEditor.getHtmlText()
+                htmlEditor.getHtmlText(),
+                attachments
         );
         emailSenderService.start();
         emailSenderService.setOnSucceeded(e->{
--- src/com/barosanu/controller/services/EmailSenderService.java
+++ src/com/barosanu/controller/services/EmailSenderService.java
@@ -5,10 +5,16 @@ import com.barosanu.model.EmailAccount;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
 import javax.mail.*;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmailSenderService extends Service<EmailSendingResult> {
 
@@ -16,12 +22,14 @@ public class EmailSenderService extends Service<EmailSendingResult> {
     private String subject;
     private String recipient;
     private String content;
+    private List<File> attachments;
 
-    public EmailSenderService(EmailAccount emailAccount, String subject, String recipient, String content) {
+    public EmailSenderService(EmailAccount emailAccount, String subject, String recipient, String content, List<File> attachments) {
         this.emailAccount = emailAccount;
         this.subject = subject;
         this.recipient = recipient;
         this.content = content;
+        this.attachments = attachments;
     }
 
     @Override
@@ -41,6 +49,16 @@ public class EmailSenderService extends Service<EmailSendingResult> {
                     messageBodyPart.setContent(content, "text/html");
                     multipart.addBodyPart(messageBodyPart);
                     mimeMessage.setContent(multipart);
+                    // adding attachments:
+                    if(attachments.size()>0){
+                        for ( File file: attachments){
+                            MimeBodyPart mimeBodyPart = new MimeBodyPart();
+                            DataSource source = new FileDataSource(file.getAbsolutePath());
+                            mimeBodyPart.setDataHandler(new DataHandler(source));
+                            mimeBodyPart.setFileName(file.getName());
+                            multipart.addBodyPart(mimeBodyPart);
+                        }
+                    }
                     //Sending the message:
                     Transport transport = emailAccount.getSession().getTransport();
                     transport.connect(
