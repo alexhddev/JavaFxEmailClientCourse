7df82ed2911641bd3a3714aea55a284e4bb8c70f
barosanuemailtest <barosanu.email.test@gmail.com>
2020-04-14 11:18:51 +0300

    Save attachments to emailMessage

--- src/com/barosanu/controller/services/MessageRendererService.java
+++ src/com/barosanu/controller/services/MessageRendererService.java
@@ -9,6 +9,7 @@ import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Multipart;
+import javax.mail.internet.MimeBodyPart;
 import java.io.IOException;
 
 public class MessageRendererService extends Service {
@@ -69,8 +70,16 @@ public class MessageRendererService extends Service {
             } else if(isMultipartType(contentType)){
                 Multipart multipart2 = (Multipart) bodyPart.getContent();
                 loadMultipart(multipart2, stringBuffer);
+            } else if(!isTextPlain(contentType)){
+                //here we get the attachments:
+                MimeBodyPart mbp = (MimeBodyPart) bodyPart;
+                emailMessage.addAttachment(mbp);
             }
         }
+    }
+
+    private boolean isTextPlain(String contentType){
+        return  contentType.contains("TEXT/PLAIN");
     }
 
     private boolean isSimpleType(String contentType){
--- src/com/barosanu/model/EmailMessage.java
+++ src/com/barosanu/model/EmailMessage.java
@@ -5,7 +5,10 @@ import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 
 import javax.mail.Message;
+import javax.mail.internet.MimeBodyPart;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class EmailMessage {
 
@@ -16,6 +19,8 @@ public class EmailMessage {
     private SimpleObjectProperty<Date> date;
     private boolean isRead;
     private Message message;
+    private List<MimeBodyPart> attachmentList = new ArrayList<MimeBodyPart>();
+    private boolean hasAttachments = false;
 
     public EmailMessage(String subject, String sender, String recipient, int size, Date date, boolean isRead, Message message){
         this.subject = new SimpleStringProperty(subject);
@@ -51,5 +56,16 @@ public class EmailMessage {
     }
     public Message getMessage(){
         return this.message;
+    }
+
+    public void addAttachment(MimeBodyPart mbp) {
+        hasAttachments = true;
+        attachmentList.add(mbp);
+        try {
+            System.out.println("Added attach: " + mbp.getFileName());
+        } catch ( Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
